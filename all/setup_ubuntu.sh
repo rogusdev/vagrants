ea186d833c9c9d8db37617d35599c4852abd3559
Chris Rogus <rogusdev@gmail.com>
2020-03-29 17:20:57 -0400

    Update setup_ubuntu.sh

--- all/setup_ubuntu.sh
+++ all/setup_ubuntu.sh
@@ -10,6 +10,9 @@ echo -e "\ncd /vagrant" >> ~/.bashrc  # ubuntu
 sudo apt-get update && sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 sudo apt-get install -yq git linux-headers-$(uname -r) build-essential libssl-dev libreadline-dev
 
+# jq required for parsing github releases lists, unzip for a bunch, libcurl + zlib for C# -- libpng just in case
+sudo apt-get install -yq jq unzip libcurl4 libcurl4-openssl-dev zlib1g-dev libpng-dev
+
 
 sudo apt-get install -yq apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
@@ -17,14 +20,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-
-# https://github.com/docker/compose/releases
-# https://docs.docker.com/compose/install/#install-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-sudo curl -L https://raw.githubusercontent.com/docker/compose/1.24.5/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+sudo apt-get install -yq docker-ce docker-ce-cli containerd.io
 
 sudo usermod -aG docker $(whoami)  # required for docker permissions! (you will need to restart your shell after this)
 
@@ -47,21 +43,56 @@ EOF
 
 
 
+
+VERSION_DOCKER_COMPOSE=1.25.4
+VERSION_RUST=1.42.0
+VERSION_DOTNET_CORE=3.1.201
+VERSION_GOLANG=1.14.1
+VERSION_JAVA=azul-zulu-11.37.17-jdk11.0.6
+VERSION_GRADLE=6.3
+VERSION_NODEJS=12.16.1
+VERSION_YARN=1.22.4
+VERSION_RUBY=2.6.5
+VERSION_PYTHON2=2.7.17
+VERSION_PYTHON3=3.8.2
+VERSION_BAZEL=2.2.0
+VERSION_TERRAFORM=0.12.14
+VERSION_HELM=2.16.5
+VERSION_KUBECTL=1.16.7
+VERSION_KOPS=v1.16.0
+VERSION_SOPS=v3.5.0
+VERSION_ERLANG=22.3
+VERSION_ELIXIR=1.10
+VERSION_POSTGRES=12.2
+VERSION_REDIS=5.0.8
+VERSION_MONGODB=4.2.5
+VERSION_ELASTICSEARCH=7.6.1
+
+
+
+
+asdf plugin-add docker-compose https://github.com/virtualstaticvoid/asdf-docker-compose.git
+asdf install docker-compose $VERSION_DOCKER_COMPOSE
+asdf global docker-compose $VERSION_DOCKER_COMPOSE
+
+sudo curl -L https://raw.githubusercontent.com/docker/compose/$VERSION_DOCKER_COMPOSE/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
+
 asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
-asdf install rust 1.42.0
-asdf global rust 1.42.0
+asdf install rust $VERSION_RUST
+asdf global rust $VERSION_RUST
 
 
 # https://github.com/dotnet/core/tree/master/release-notes
 asdf plugin-add dotnet-core https://github.com/emersonsoares/asdf-dotnet-core.git
-asdf install dotnet-core 3.1.201
-asdf global dotnet-core 3.1.201
+asdf install dotnet-core $VERSION_DOTNET_CORE
+asdf global dotnet-core $VERSION_DOTNET_CORE
 
 
 # https://golang.org/dl/
 asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
-asdf install golang 1.14.1
-asdf global golang 1.14.1
+asdf install golang $VERSION_GOLANG
+asdf global golang $VERSION_GOLANG
 # echo -e '\nGOPATH=$HOME/go' >> ~/.bashrc
 asdf reshim golang
 
@@ -73,34 +104,34 @@ echo -e "\nsource ~/.asdf/plugins/java/set-java-home.sh" >> ~/.bashrc
 #source ~/.asdf/plugins/java/set-java-home.fish
 
 asdf plugin-add java https://github.com/halcyon/asdf-java.git
-asdf install java azul-zulu-11.37.17-jdk11.0.6
-asdf global java azul-zulu-11.37.17-jdk11.0.6
+asdf install java $VERSION_JAVA
+asdf global java $VERSION_JAVA
 
 
 # https://gradle.org/releases/
 asdf plugin-add gradle https://github.com/rfrancis/asdf-gradle.git
-asdf install gradle 6.3
-asdf global gradle 6.3
+asdf install gradle $VERSION_GRADLE
+asdf global gradle $VERSION_GRADLE
 
 
 
 
 # linux nodejs
-sudo apt-get install -yq dirmngr gpg
+#sudo apt-get install -yq dirmngr gpg  # already on ubuntu
 
 # mac nodejs
 #brew install coreutils gpg
 
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
-asdf install nodejs 12.16.1
-asdf global nodejs 12.16.1
+asdf install nodejs $VERSION_NODEJS
+asdf global nodejs $VERSION_NODEJS
 asdf reshim nodejs
 
 
 asdf plugin-add yarn
-asdf install yarn 1.22.4
-asdf global yarn 1.22.4
+asdf install yarn $VERSION_YARN
+asdf global yarn $VERSION_YARN
 
 
 # linux ruby-build
@@ -111,75 +142,75 @@ sudo apt-get install -yq autoconf bison build-essential libssl-dev libyaml-dev l
 #brew install openssl libyaml libffi
 
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
-asdf install ruby 2.6.5
-asdf global ruby 2.6.5
+asdf install ruby $VERSION_RUBY
+asdf global ruby $VERSION_RUBY
 gem install bundler
 asdf reshim ruby
 
 
 # linux python dependencies
 # https://github.com/pyenv/pyenv/wiki#suggested-build-environment
-sudo apt-get update; sudo apt-get install -yq --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt-get install -yq --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 # https://www.python.org/downloads/
 asdf plugin-add python
-asdf install python 2.7.17
-asdf install python 3.8.2
-asdf global python 3.8.2 2.7.17
+asdf install python $VERSION_PYTHON2
+asdf install python $VERSION_PYTHON3
+asdf global python $VERSION_PYTHON3 $VERSION_PYTHON2
 pip install --upgrade pip
 asdf reshim python
 
 
 asdf plugin-add bazel https://github.com/rajatvig/asdf-bazel.git
-asdf install bazel 2.2.0
-asdf global bazel 2.2.0
+asdf install bazel $VERSION_BAZEL
+asdf global bazel $VERSION_BAZEL
 
 
 
 
 asdf plugin-add terraform https://github.com/Banno/asdf-hashicorp.git
-asdf install terraform 0.12.12
-asdf global terraform 0.12.12
+asdf install terraform $VERSION_TERRAFORM
+asdf global terraform $VERSION_TERRAFORM
 
 
 asdf plugin-add helm https://github.com/Antiarchitect/asdf-helm.git
-asdf install helm 2.16.5
-asdf global helm 2.16.5
+asdf install helm $VERSION_HELM
+asdf global helm $VERSION_HELM
 
 
 asdf plugin-add kubectl https://github.com/Banno/asdf-kubectl.git
-asdf install kubectl 1.16.7
-asdf global kubectl 1.16.7
+asdf install kubectl $VERSION_KUBECTL
+asdf global kubectl $VERSION_KUBECTL
 
 
 asdf plugin-add kops https://github.com/Antiarchitect/asdf-kops.git
-asdf install kops v1.16.0
-asdf global kops v1.16.0
+asdf install kops $VERSION_KOPS
+asdf global kops $VERSION_KOPS
 
 
 asdf plugin-add sops https://github.com/feniix/asdf-sops.git
-asdf install sops v3.5.0
-asdf global sops v3.5.0
+asdf install sops $VERSION_SOPS
+asdf global sops $VERSION_SOPS
 
 
 
-sudo apt-get install -yq unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && sudo ./aws/install
 
 
 
+# somehow erlang requires java??  pass.  also takes a realyl long time to build
 
-# linux erlang
-sudo apt-get install -yq build-essential autoconf m4 libncurses5-dev libwxgtk3.0-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop
+# # linux erlang
+# #sudo apt-get install -yq build-essential autoconf m4 libncurses5-dev libwxgtk3.0-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop
 
-asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
-asdf install erlang 22.3
-asdf global erlang 22.3
+# asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
+# asdf install erlang $VERSION_ERLANG
+# asdf global erlang $VERSION_ERLANG
 
 
-asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
-asdf install elixir 1.10
-asdf global elixir 1.10
+# asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+# asdf install elixir $VERSION_ELIXIR
+# asdf global elixir $VERSION_ELIXIR
 
 
 
@@ -191,35 +222,32 @@ cd /tmp && curl http://download.redis.io/redis-stable.tar.gz | tar xz \
  && make -C redis-stable redis-cli && sudo cp redis-stable/src/redis-cli /usr/local/bin \
  && rm -rf /tmp/redis-stable && cd -
 
+# or alternatively use netcat for redis client:
+#nc -v --ssl redis.mydomain.com 6380
 
 
-
-# linux postgres + redis
-sudo apt install -yq linux-headers-$(uname -r) build-essential
-
-# linux postgres
-sudo apt install -yq libreadline-dev zlib1g-dev curl
-
-asdf plugin-add postgres
-asdf install postgres 12.2
-asdf global postgres 12.2
-
-sudo apt-get install -yq libpq-dev pgadmin4
+# sudo apt-get install -yq libpq-dev pgadmin4 postgresql-client mongodb-org-tools
 
 
-asdf plugin-add redis https://github.com/smashedtoatoms/asdf-redis.git
-asdf install redis 5.0.8
-asdf global redis 5.0.8
+# asdf plugin-add postgres
+# asdf install postgres $VERSION_POSTGRES
+# asdf global postgres $VERSION_POSTGRES
 
 
-asdf plugin-add mongodb https://github.com/sylph01/asdf-mongodb.git
-asdf install mongodb 4.3.5
-asdf global mongodb 4.3.5
+# asdf plugin-add redis https://github.com/smashedtoatoms/asdf-redis.git
+# asdf install redis $VERSION_REDIS
+# asdf global redis $VERSION_REDIS
 
 
-asdf plugin-add elasticsearch https://github.com/asdf-community/asdf-elasticsearch.git
-asdf install elasticsearch 7.6.1
-asdf global elasticsearch 7.6.1
+# mongo asdf plugin not working as of 2020-03-29
+# asdf plugin-add mongodb https://github.com/sylph01/asdf-mongodb.git
+# asdf install mongodb $VERSION_MONGODB
+# asdf global mongodb $VERSION_MONGODB
+
+
+# asdf plugin-add elasticsearch https://github.com/asdf-community/asdf-elasticsearch.git
+# asdf install elasticsearch $VERSION_ELASTICSEARCH
+# asdf global elasticsearch $VERSION_ELASTICSEARCH
 
 
 
@@ -236,15 +264,28 @@ docker-compose --version
 aws --version
 ruby --version
 python --version
+python2 --version
+python3 --version
 pip --version
 java -version
 gradle --version
 psql --version
 go version
-rust --version
-erlang --version
-elixir --version
+rustc --version
+# erlang --version
+# elixir --version
 heroku --version
+terraform --version
+helm version
+kubectl version
+kops version
+sops --version
+redis-cli --version
+psql --version
+mongo --version
+bazel --version
+
+
 
 echo -e "\n\n"
 
