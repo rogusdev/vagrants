6aedb3aa8c16acc5fc600b665dd58b0a4ca0c3be
Chris Rogus <rogusdev@gmail.com>
2020-09-21 18:43:17 -0400

    fix sed

--- elementary.sh
+++ elementary.sh
@@ -3,5 +3,5 @@ sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y curl s
   && echo -e "#!/bin/sh -e\n\nmount -t vboxsf -o rw,uid=$USER,gid=$USER vbshared /mnt/vbshared\n\nexit 0" | sudo tee -a /etc/rc.local && sudo chmod +x /etc/rc.local \
   && curl https://raw.githubusercontent.com/rogusdev/vagrants/master/all/setup_ubuntu.sh \
   && sed 's|echo -e "\ncd /vagrant" >> $PROFILE_FILE||' \
-  && sed 'UBUNTU_CODENAME=$(lsb_release -cs)|UBUNTU_CODENAME=$(lsb_release -ucs)|' \
+  && sed 's|UBUNTU_CODENAME=$(lsb_release -cs)|UBUNTU_CODENAME=$(lsb_release -ucs)|' \
   | sh
