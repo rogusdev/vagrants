11ac3b5ddc03948564f0488b40c7322507fff745
Chris Rogus <rogusdev@gmail.com>
2020-09-21 18:39:26 -0400

    Add script to use all to setup new elementary vbox

---
+++ elementary.sh
@@ -0,0 +1,7 @@
+# https://vpsfix.com/community/server-administration/no-etc-rc-local-file-on-ubuntu-18-04-heres-what-to-do/
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y curl sed \
+  && echo -e "#!/bin/sh -e\n\nmount -t vboxsf -o rw,uid=$USER,gid=$USER vbshared /mnt/vbshared\n\nexit 0" | sudo tee -a /etc/rc.local && sudo chmod +x /etc/rc.local \
+  && curl https://raw.githubusercontent.com/rogusdev/vagrants/master/all/setup_ubuntu.sh \
+  && sed 's|echo -e "\ncd /vagrant" >> $PROFILE_FILE||' \
+  && sed 'UBUNTU_CODENAME=$(lsb_release -cs)|UBUNTU_CODENAME=$(lsb_release -ucs)|' \
+  | sh
