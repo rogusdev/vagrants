d4533374c762a9342c3fa1dd8eb327db940b7721
Chris Rogus <rogusdev@gmail.com>
2020-09-21 19:08:07 -0400

    Try once more to get this to work

--- elementary.sh
+++ elementary.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # https://vpsfix.com/community/server-administration/no-etc-rc-local-file-on-ubuntu-18-04-heres-what-to-do/
 echo -e '#!/bin/sh -e'"\nmount -t vboxsf -o rw,uid=$USER,gid=$USER vbshared /mnt/vbshared\n\nexit 0" | sudo tee -a /etc/rc.local && sudo chmod +x /etc/rc.local
 
@@ -6,4 +8,4 @@ sudo apt-get update && sudo apt-get upgrade -y
 curl https://raw.githubusercontent.com/rogusdev/vagrants/master/all/setup_ubuntu.sh \
   | sed 's|echo -e "\\ncd /vagrant" >> $PROFILE_FILE|#\0|' \
   | sed 's|UBUNTU_CODENAME=$(lsb_release -cs)|UBUNTU_CODENAME=$(lsb_release -ucs)|' \
-  > setup.sh
+  | bash
